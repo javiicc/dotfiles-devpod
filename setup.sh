c81976584271a72a68b4089436d27d9fcc336527
Javier <82763996+javiicc@users.noreply.github.com>
2025-03-20 19:05:55 +0100

    feat: add kubectx

--- setup.sh
+++ setup.sh
@@ -11,28 +11,35 @@ sudo apt update
 sudo apt install -y apt-transport-https ca-certificates curl gnupg
 
 
+# --------- Install direnv ---------
+
+sudo apt -y install direnv
+command -v direnv &>/dev/null && echo "direnv installation successful!" || echo "direnv installation failed"
+
+
 # --------- Install kubectl last version ---------
 
 echo "Installing kubectl..."
 
-# Install kubectl - apt
-# if [ ! -d "/etc/apt/keyrings" ]; then
-#   sudo mkdir -p -m 755 /etc/apt/keyrings
-# fi
+# --- Install kubectl - apt package manager ---
+if [ ! -d "/etc/apt/keyrings" ]; then
+  sudo mkdir -p -m 755 /etc/apt/keyrings
+fi
 
-# curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.32/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-# sudo chmod 644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+# Download the public signing key for the Kubernetes package repositories
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.32/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+sudo chmod 644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 
-# # This overwrites any existing configuration in /etc/apt/sources.list.d/kubernetes.list
-# echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.32/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
-# sudo chmod 644 /etc/apt/sources.list.d/kubernetes.list   # helps tools such as command-not-found to work correctly
+# Add the appropriate Kubernetes apt repository
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.32/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo chmod 644 /etc/apt/sources.list.d/kubernetes.list   # helps tools such as command-not-found to work correctly
 
-# sudo apt-get update
-# sudo apt-get install -y kubectl
+sudo apt-get update
+sudo apt-get install -y kubectl
 
-# Install kubectl - binary
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/arm64/kubectl"
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+# # --- Install kubectl - binary file ---
+# curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/arm64/kubectl"
+# sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
 # Check kubectl installation
 if command -v kubectl &>/dev/null; then
@@ -43,10 +50,11 @@ else
 fi
 
 
-# --------- Install direnv ---------
+# --------- Install kubectx ---------
 
-sudo apt -y install direnv
-command -v direnv &>/dev/null && echo "direnv installation successful!" || echo "direnv installation failed"
+sudo apt install kubectx
+
+command -v kubectx &>/dev/null && echo "kubectx installation successful!" || echo "kubectx installation failed"
 
 
 # --------- Create symlinks ---------
